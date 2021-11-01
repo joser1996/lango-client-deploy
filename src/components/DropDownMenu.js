e807a6ec3e863a4af523158b6427fd49011bf4b6
Jose Torres <josertrs@gmail.com>
2021-10-31 17:51:47 -0700

    Added drop down menu

--- src/components/DropDownMenu.js
+++ src/components/DropDownMenu.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { CSSTransition } from "react-transition-group";
 
 export default function DropDownMenu() {
+    const [activeMenu, setactiveMenu] = useState('main');
+
+    function DropDownItem(props) {
+        return(
+            <a href="#" className="menu-item" >
+                {props.children}
+            </a>
+        );
+    }
+
     return (
-        <div>
-            
+        <div className="dropdown">
+            <CSSTransition
+                in={activeMenu === "main"}
+                unmountOnExit
+                timeout={500}
+                classNames="menu-primary"
+            >
+                <div className="menu">
+                    <DropDownItem>Change Language</DropDownItem>
+                    <DropDownItem>Change Deck</DropDownItem>
+                </div>
+            </CSSTransition>
         </div>
     )
 }
--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function NavItem(props) {
+    const [open, setOpen] = useState(false);
+
     return (
         <li className="nav-item">
-            <a href="#" className="icon-button" title={props.desc}>
+            <a href="#" className="icon-button" title={props.desc} onClick={() => setOpen(!open)}>
                 {props.icon}
             </a>
+
+            {open && props.children}
         </li>
     )
 }
